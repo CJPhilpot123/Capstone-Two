099eebcbde38b0ad945c99e78f2cf265a31f6095
Chris Phillips <you@example.com>
2019-05-07 05:44:48 -0400

    Capstone 2

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Task_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task();
+            List<Task> Tasks = new List<Task>();
+            Tasks.Add(new Task("Chris", "Clean Classroom", "5/3/2019", false));
+            int wave = 1;
+            while (wave == 1)
+            {
+                Console.WriteLine("Welcome to the Task Manager Pro!\n     (1)List Task\n     (2)Add Task\n     (3)Delete Task\n     (4)Mark Task Complete\n     (5)Quit\nWhat would you like to do?");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine("LISTING:");
+                    foreach (Task t in Tasks)
+                    {
+                        Task.LISTtask(t);
+                    }
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine("ADD TASK:\n");
+                    Task.ADDTask(Tasks);
+                    Console.WriteLine("Task Entered!");
+                }
+                if (choice == 3)
+                {
+                    foreach (Task t in Tasks)
+                    {
+                        Task.LISTtask(t);
+                    }
+                    Task.DeleteTask(Tasks);
+                }
+                if ( choice == 4)
+                {
+                    foreach (Task t in Tasks)
+                    {
+                        Task.FalsetoTrue(t);
+                    }
+                }
+                if (choice == 5)
+                {
+                    Console.WriteLine("Hope You Have A Nice Day!");
+                    wave--;
+                }
+            }
+
+
+        }
+
+       
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Task_List
+{
+    class Task
+    {
+        private string name;
+        private string brief;
+        private string date;
+        private bool completion;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Brief
+        {
+            set
+            {
+                brief = value;
+            }
+            get
+            {
+                return brief;
+            }
+        }
+        public string Date
+        {
+            set
+            {
+                date = value;
+            }
+            get
+            {
+                return date;
+            }
+        }
+        public bool Complete
+        {
+            set
+            {
+                completion = value;
+            }
+            get
+            {
+                return completion;
+            }
+        }
+
+
+
+        public Task(string TeamName, string Descript, string DueDate, bool Completion)
+        {
+            name = TeamName;
+            brief = Descript;
+            date = DueDate;
+            completion = Completion;
+        }
+
+        public Task() { }
+
+        public static void ADDTask(List<Task> Tasks)
+        {
+            Task task = new Task();
+            Console.WriteLine("Enter Team Member Name:");
+            task.name = Console.ReadLine();
+            Console.WriteLine("Enter Description of Task:");
+            task.brief = Console.ReadLine();
+            Console.WriteLine("Enter Due Date of Task:");
+            task.date = Console.ReadLine();
+            Console.WriteLine("Is it completed? (true/false)");
+            task.completion = bool.Parse(Console.ReadLine());
+            Tasks.Add(task);
+        }
+
+
+        public static void LISTtask(Task t)
+        {
+            Console.WriteLine($"\nName:{t.name}  \nTask Descript:{t.brief}  \nDue Date:{t.date} \nIs task completed? {t.completion}");
+        }
+
+        public static void DeleteTask(List<Task> Tasks)
+        {
+            try
+            {
+                Console.WriteLine("What would you like to delete: ");
+                int indexOf = int.Parse(Console.ReadLine());
+
+                indexOf--;
+
+                Tasks.RemoveAt(indexOf);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Item not found");
+                DeleteTask(Tasks);
+            }
+        }
+        public static void FalsetoTrue(Task t)
+        {
+            try
+            {
+                Console.WriteLine("Which task have you completed? (Enter Name of Teammember)");
+                string answer = Console.ReadLine();
+                if (t.name == answer)
+                
+                    t.completion = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Item not found");
+                FalsetoTrue(t);
+            }
+        }
+    }
+}
